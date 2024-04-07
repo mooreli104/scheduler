0a4e1bf25baaf1f3dc9a0a82058f63881e72afef
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2024-04-06 22:50:19 -0400

    Simplify JS for drag and drop elements.

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-const listContainer = document.getElementById("list-container");
 
-function requirementCompleted(){
+let lists = document.getElementsByClassName("list");
+let listContainer = document.getElementById("list-container");
+let year = document.querySelectorAll(".year, #right, #left-completed");
 
-    
+function saveData(){
+    localStorage.setItem("data");
 }
+
+
+for(list of lists){
+    list.addEventListener("dragstart", function(e){
+        let selected = e.target;
+
+        year.forEach(function(item){
+            item.addEventListener("dragover", function(e){
+                e.preventDefault();
+            })
+            item.addEventListener("drop", function(e){
+                item.appendChild(selected);
+                selected = null;
+            })
+        });
+    })
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+}, false);
+
