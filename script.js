eb59fc6151c7b88709b7c70ef55aeb46ef876bc4
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2024-04-07 15:57:15 -0400

    Made dragging classes into a semester "complete" requirements.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 
 let lists = document.getElementsByClassName("list");
 let listContainer = document.getElementById("list-container");
-let year = document.querySelectorAll(".year, #right, #left-completed");
+let dragAndDrop = document.querySelectorAll(".semester, #right, #left-completed");
+let required = document.querySelectorAll("#task-1");
+let semester = document.querySelectorAll(".semester");
 
-function saveData(){
-    localStorage.setItem("data");
+
+function completeRequirement(){
+ required.forEach(function(item){
+    item.classList.toggle("checked");
+ });   
 }
 
 
@@ -12,21 +17,37 @@ for(list of lists){
     list.addEventListener("dragstart", function(e){
         let selected = e.target;
 
-        year.forEach(function(item){
+        dragAndDrop.forEach(function(item){
             item.addEventListener("dragover", function(e){
                 e.preventDefault();
             })
+            
             item.addEventListener("drop", function(e){
                 item.appendChild(selected);
                 selected = null;
+
+               completeRequirement();
+
             })
         });
     })
 }
+// if(semester.classList){
+//     required.classList.toggle("checked"); 
+// }
 
-listContainer.addEventListener("click",function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-    }
-}, false);
+// listContainer.addEventListener("click",function(e){
+//     if(e.target.tagName === "LI"){
+//         e.target.classList.toggle("checked");
+//     }
+// }, false);
+
+//TRYING TO SAVE THE CLASSES THAT WERE DROPPED INTO THE SEMESTERS
+// function saveData(){
+//     localStorage.setItem("data", autumn.innerHTML);
+// }
+
+// function showClass(){
+//     autumn.innerHTML = localStorage.getItem("data");
+// }
 
