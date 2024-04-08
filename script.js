c4293d706a155abb4fe303f5122e84a61d7a42a3
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2024-04-08 19:03:18 -0400

    Finish basic requirements functionality

--- script.js
+++ script.js
@@ -2,51 +2,74 @@
 let lists = document.getElementsByClassName("list");
 let listContainer = document.getElementById("list-container");
 let dragAndDrop = document.querySelectorAll(".semester, #right, #left-completed");
-let required = document.querySelectorAll("#task-1");
-let middle = document.getElementById("middle");
 //With querySelectorALl, must loop over elements (like an array of each element with .semester class
-let semester = document.querySelectorAll(".semester");
 let labels = document.querySelectorAll(".label");
 
-function completeRequirement(){
- required.forEach(function(item){
-    let arr = middle.getElementsByClassName("list");
-        if(arr.length>0){
-            item.classList.add("checked");
-        
-        }
-        else{
-            item.classList.remove("checked");
-        }
- });   
+
+
+function completeBigRequirement() {
+    let graduation = document.getElementById("graduation");
+    let total = graduation.getElementsByClassName("specific");
+    let checks = graduation.getElementsByClassName("checked");
+    if (total.length === checks.length) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
+function complete() {
+    if (completeBigRequirement()) {
+        graduation.classList.add("checked");
+    }
+    else {
+        graduation.classList.remove("checked");
+    }
+}
 
-for(list of lists){
-    list.addEventListener("dragstart", function(e){
-        let selected = e.target;
-        dragAndDrop.forEach(function(item){
-            item.addEventListener("dragover", function(e){
-                e.preventDefault();
-            })
-            
-            item.addEventListener("drop", function(e){
-                if(selected!=null){
-                item.appendChild(selected);
-                completeRequirement();
-                }
-                selected = null;
-                
-            })
-        });
-        
+function completeRequirement() {
+    let required = document.querySelectorAll("#task-1");
+    required.forEach(function (item) {
+        let arr = middle.getElementsByClassName("list");
+        if (arr.length > 0) {
+            item.classList.add("checked");
+
+        }
+        else {
+            item.classList.remove("checked");
+        }
     });
 }
 
-labels.forEach(function(item){
-    item.addEventListener("click", function(e){
+
+for (list of lists) {
+    list.addEventListener("dragstart", function (e) {
+        let selected = e.target;
+        dragAndDrop.forEach(function (item) {
+            item.addEventListener("dragover", function (e) {
+                e.preventDefault();
+            })
+
+            item.addEventListener("drop", function (e) {
+                if (selected != null) {
+                    item.appendChild(selected);
+                    completeRequirement();
+                    complete();
+                }
+                selected = null;
+
+            })
+        });
+
+    });
+}
+
+labels.forEach(function (item) {
+    item.addEventListener("click", function (e) {
         let content = document.getElementById("test");
         content.classList.toggle("fulfilled");
+        item.classList.toggle("fulfilled");
     });
 
 });
